398183cc25cab66185236e07b8741940da44fc71
jianglongnie <jianglongnie@outlook.com>
2025-09-08 16:04:47 +0800

    feat: Add new hash function

--- contrib/atp/model/atp-aggregator.h
+++ contrib/atp/model/atp-aggregator.h
@@ -4,6 +4,7 @@
 #include "ns3/packet.h"
 #include "ns3/ptr.h"
 #include "atp-tag.h"
+#include <boost/functional/hash.hpp>
 
 #include <cstdint>
 
@@ -31,11 +32,21 @@ public:
     void Reset();
 
     // 添加静态哈希函数
+    
     static std::size_t HashToIndex(uint8_t jobId, uint32_t seqNum, uint32_t MAX_AGGREGATORS) {
         std::size_t h1 = std::hash<uint8_t>()(jobId);
         std::size_t h2 = std::hash<uint32_t>()(seqNum);
         return ((h1 << 1) ^ h2) % MAX_AGGREGATORS;
     }
+    /*
+    static std::size_t HashToIndex(uint8_t jobId, uint32_t seqNum, uint32_t MAX_AGGREGATORS) {
+        std::size_t seed = 0;
+        boost::hash_combine(seed, jobId);
+        boost::hash_combine(seed, seqNum);
+        return seed % MAX_AGGREGATORS;
+    }
+    */
+        
 };
 
 } // namespace ns3
